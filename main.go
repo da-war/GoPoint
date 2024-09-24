3591fe262bbd2164836bfbee4fd47d1babbd1d2a
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-24 07:12:27 +0500

    Pointer

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Code
+	age := 32
+	hello := &age
+	fmt.Println("Age:", age)
+	fmt.Println("Hello:", hello)
+	fmt.Print("adultYear:", adultYear(age))
+}
+
+func adultYear(age int) int {
+	return age - 18
 }
